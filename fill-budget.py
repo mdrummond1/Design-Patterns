f232cb668437bea7e35ae48a2563269fc397d761
Matthew Drummond <mattatatter@gmail.com>
2020-07-29 22:17:48 -0500

    added functions to login, click element, input_dates

--- fill-budget.py
+++ fill-budget.py
@@ -1,17 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from functions import webdriver
+from functions import EC
+from functions import By
+from functions import time
+from functions import WebDriverWait
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import os
-import getpass
 import functions
-import time
 
 
 
@@ -20,42 +17,30 @@ import time
 #research requests to see if it's useful
 
 #TODO: Use cryptography to save username/password
-
 #key = Fernet.generate_key()
 
 url = "https://cuaonline.cuofamerica.com/MyAccountsV2"
-
 PATH = "./browserDrivers/chromedriver.exe"
-#PATH = "/mnt/c/Program Files (x86)/chromedriver.exe"
 
-#get bank login info
-user = input("Enter UserName: ")
-password = getpass.getpass("Enter password: ")
 
+log_info = functions.get_log_info()#get bank login info
 
 driver = webdriver.Chrome(PATH)
 driver.get(url)
 
-#login
-username = WebDriverWait(driver, 5).until(lambda d:d.find_element_by_id("UserName"))
-username.send_keys(user)
-passwd = driver.find_element_by_id("Password")
-passwd.send_keys(password + Keys.RETURN)
-
+functions.login(log_info[0], log_info[1] , driver)#login
 
 i = 1#cause apparently I need a counter for the dropbox list
 
 #get list of accounts
 accounts = WebDriverWait(driver, 5).until(lambda d: d.find_elements_by_class_name("account"))
 
-date_params = functions.get_date_parameters()
+date_params = functions.get_date_parameters()#generate dates
 
 for account in accounts:#download csv for each account
     account.click()
-    
-    download_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'export_trigger')))
-    time.sleep(0.5)
-    download_btn.click()#click download
+
+    functions.wait_and_click('export_trigger', driver, 5)    
     
     drop = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "x-form-text")))
     drop.click()#open dropbox
@@ -63,23 +48,25 @@ for account in accounts:#download csv for each account
     dropbox_items = WebDriverWait(driver, 5).until(lambda d: d.find_elements(By.CLASS_NAME,"x-combo-list-item"))
     dropbox_items[i].click()#select csv
 
-    #TODO: something with the dates is broken
-    start = driver.find_element_by_id("Parameters_StartDate")
-    driver.execute_script(date_params['start_date'], start)
-    end = driver.find_element_by_id("Parameters_EndDate")
-    driver.execute_script(date_params['end_date'], end)
-
-    confirm_btn = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "export_transactions_confirm_button")))
-    confirm_btn.click()#confirm date and format
-    time.sleep(3)
-
-    confirm_btn.send_keys(Keys.ESCAPE)#close side panel to get ready for next account
+    functions.input_dates(date_params, driver)#put date values into date selectors
+        
+    #click confirm
+    confirm_btn = functions.wait_and_click("export_transactions_confirm_button", driver, 5)
     
-    time.sleep(3)
+    time.sleep(2)#give the file time to download
+
+    confirm_btn.send_keys(functions.Keys.ESCAPE)#close side panel to get ready for next account
+    
+    time.sleep(1)#give the sidebar time to close
     
     i = i + 5 #increment counter for dropbox
 
-
 driver.quit()    
 
-#process csv's
+#TODO: process csv's
+
+#path to downloads C:\Users\matta\Downloads
+#os.system("c:")
+#os.system("cd ")
+#os.system("rm C:/Users/matta/Downloads/*.csv")
+#os.system("del *.csv'")
