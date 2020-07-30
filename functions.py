5eeaf43376cdf958300b19d3dfa29c21dd5533dd
Matthew Drummond <mattatatter@gmail.com>
2020-07-29 22:18:23 -0500

    moved imports to functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
 #module for functions
 from calendar import monthlen
 import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import getpass
 
 def get_date_parameters():
     '''get_date_parameters()-> dict
@@ -40,10 +46,49 @@ def get_date_parameters():
     }
     return date
 
+def get_log_info():
+    log = []
+    log.append(input("Enter User Name: "))
+    log.append(getpass.getpass("Enter password: "))
+    return log
 
-def check_day(yr, mo, day):
-    last_day = monthlen(yr, mo)
-    if day > last_day:
-        return last_day
-    else:
-        return day
+def login(user, password, driver):
+    '''login(user, password, driver) -> void
+        takes a username/password combo and driver
+        finds login element and insert login info
+    '''
+    username = WebDriverWait(driver, 5).until(lambda d:d.find_element_by_id("UserName"))
+    username.send_keys(user)
+    passwd = driver.find_element_by_id("Password")
+    passwd.send_keys(password + Keys.RETURN)    
+
+def wait_and_click(id, driver, tm):
+    '''wait_and_click(id, driver, time) -> void
+        Takes an id, driver and time.
+        User driver to search page for id with timeout of tm
+    '''
+    elem = WebDriverWait(driver, tm).until(EC.element_to_be_clickable((By.ID, id)))
+    time.sleep(.5)
+    elem.click()
+    return elem
+
+def input_dates(date, driver):
+    '''input_dates(date, driver) -> void
+    takes in a dictionary of start and end date
+    and inputs them into the value of the correct elements
+    end date is todays date
+    '''
+    start = driver.find_element_by_id("Parameters_StartDate")
+    driver.execute_script(date['start_date'], start)
+    end = driver.find_element_by_id("Parameters_EndDate")
+    driver.execute_script(date['end_date'], end)
+
+#use dictionary or tuple to tell function what we want to do
+'''switch(choice){
+
+    case wait_id:
+    case wait_class
+    case wait_id_click
+    case wait_class_click
+}
+'''
