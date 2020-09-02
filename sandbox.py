c716b375d61b3c6e73d484a16d2ecf2de8648da8
Test <mattatatter@gmail.com>
2020-09-01 20:16:00 -0500

    tested cleaing description and reading account order

--- sandbox.py
+++ sandbox.py
@@ -6,6 +6,8 @@ import csv
 from winreg import QueryValueEx, OpenKey, HKEY_CURRENT_USER
 from os import listdir, remove
 from os.path import isfile, join
+from transaction import Transaction
+from functions import *
 
 
 #===============Testing requests and beautiful soup==================
@@ -38,7 +40,6 @@ csvs = [f for f in listdir(path_to_csvs) if isfile(join(path_to_csvs, f)) and 'E
 readers = []
 row_to_remove = 0
 
-print(csvs)
 
 for file in csvs:
     fl = open(path_to_csvs + file)
@@ -52,26 +53,28 @@ for file in csvs:
 mPaycheck = []
 mPayAmt = 0
 bPaycheck = []
+
 bPayAmt = 0
 
+order = functions.get_account_order()
+print(str(order))
 
-
-t = []
+t = clean_rows(readers)
 #test how to process the csv data
- 
+
 #This is how we can clean the descriptions. Hopefully we can eventually change it so we don't need 3 loops
-for row in readers:
+""" for row in readers:
     for cat in functions.categories.values():
         for sub in cat:
             if sub.lower() in row[functions.csv_fields['desc']].lower():
                 row[functions.csv_fields['desc']] = sub
                 t.append(Transaction(row))
                 break
-    
+ """    
 
-""" for trans in t:
+for trans in t:
     trans.display()
- """
+
 """ 
 
 print("Britt Pay: " + str(bPayAmt))
